3473740247545f0a23c9999a6833e5cd3f5b6ff9
pole4kaaa <neza56grn@gmail.com>
2019-01-21 15:20:38 +0200

    21.01

---
+++ numbers1/src/pack1/Main.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int x = 67;
+        int y = 3;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
